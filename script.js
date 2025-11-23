bce709693201578d011c2421e636abe304721508
Hemanta279 <sahuhemantakumar67@gmail.com>
2025-11-24 02:03:18 +0530

    final initial commit

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 console.log("Welcome to spotify");
 
 // Initialize the variable
+
+let songs = [
+    {songName:"Perfect", filepath:"songs/1Perfect.mpeg", coverpath:"cover/1Perfect.jpg"},
+    {songName:"Shape of you", filepath:"songs/2Shape of you.mpeg", coverpath:"cover/2Shape of you.jpg"},
+    {songName:"Sapphire", filepath:"songs/3Sapphire.mpeg", coverpath:"cover/3Sapphire.jpg"},
+    {songName:"Thinking out loud", filepath:"songs/4Thinking out loud.mpeg", coverpath:"cover/4Thinking out loud.jpg"},
+    {songName:"Photograph", filepath:"songs/5Photograph.mpeg", coverpath:"cover/5photograph.jpg"},
+    {songName:"Shivers", filepath:"songs/6Shivers.mpeg", coverpath:"cover/6Shivers.jpg"},
+
+]
+
 let songIndex = 0;
-// let audioElement = new Audio('songs/1Perfect.mpeg');
-let audioElement = new Audio();
-audioElement.src = songs[0].filepath;
+let audioElement = new Audio('songs/1Perfect.mpeg');
+// let audioElement = new Audio();
+// audioElement.src = songs[0].filepath;
+// masterSongName.innerText = songs[0].songName;
+
+// let audioElement = new Audio(songs[0].filepath);
+// let songIndex = 0;
+
+// masterSongName.innerText = songs[0].songName;
+
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
 let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
-let songs = [
-    {songName:"Perfect", filepath:"songs/1Perfect.mpeg", coverpath:"cover/1Perfect.jpg"},
-    {songName:"Shape of you", filepath:"songs/2Shape of you.mpeg", coverpath:"cover/1.jpg"},
-    {songName:"Sapphire", filepath:"songs/3Sapphire.mpeg", coverpath:"cover/1.jpg"},
-    {songName:"Thinking out loud", filepath:"songs/4Thinking out loud.mpeg", coverpath:"cover/1.jpg"},
-    {songName:"Photograph", filepath:"songs/5Photograph.mpeg", coverpath:"cover/1.jpg"},
-    {songName:"Shivers", filepath:"songs/6Shivers.mpeg", coverpath:"cover/1.jpg"},
 
-]
+
+masterSongName.innerText = songs[0].songName;
 
 songItems.forEach((element,i) =>{
     // console.log(element,i);
@@ -89,7 +101,7 @@ document.getElementById('next').addEventListener('click', ()=>{
     else{
         songIndex += 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mpeg`;
+    audioElement.src = songs[songIndex].filepath;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
@@ -105,7 +117,7 @@ document.getElementById('previous').addEventListener('click', ()=>{
     else{
         songIndex -= 1;
     }
-    audioElement.src = `songs/${songIndex+1}.mpeg`;
+    audioElement.src = songs[songIndex].filepath;
     masterSongName.innerText = songs[songIndex].songName;
     audioElement.currentTime = 0;
     audioElement.play();
@@ -125,76 +137,8 @@ songs.forEach((song, index) => {
         let minutes = Math.floor(totalSeconds / 60);
         let seconds = totalSeconds % 60;
 
-        // Format 5:04 → 05:04
         if (seconds < 10) seconds = "0" + seconds;
 
         durationElements[index].childNodes[0].textContent = `${minutes}:${seconds}`;
     });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log("Welcome to spotify");
-
-// // Initialize the variables
-// let songIndex = 0;
-// // *** Ensure this path/filename is correct ***
-// let audioElement = new Audio('Perfect.mpeg'); 
-// let masterPlay = document.getElementById('masterPlay');
-// let myProgressBar = document.getElementById('myProgressBar');
-// let gif = document.getElementById('gif');
-
-// // Define songs array (using the first song as a demo)
-// let songs = [
-//     {songName:"Perfect", filepath:"song/Perfect.mpeg", coverpath:"cover/1.jpg"},
-//     // ... other songs will go here
-// ]
-
-// // 1. Handle play/pause click (Your existing correct logic)
-// masterPlay.addEventListener('click', ()=>{
-//     if (audioElement.paused || audioElement.currentTime <= 0) {
-//         audioElement.play();
-//         masterPlay.classList.remove('fa-play-circle');
-//         masterPlay.classList.add('fa-pause-circle');
-//         gif.style.opacity = 1;
-//     }
-//     else {
-//         audioElement.pause();
-//         masterPlay.classList.remove('fa-pause-circle');
-//         masterPlay.classList.add('fa-play-circle');
-//         gif.style.opacity = 0;
-//     }
-// });
-
-// // 2. Listen to timeupdate for Progress Bar (FIXED & ADDED)
-// audioElement.addEventListener('timeupdate', ()=>{
-//     // Update Seekbar value based on percentage of song played
-//     let progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
-//     myProgressBar.value = progress;
-// });
-
-// // 3. Handle seeking via Progress Bar (NEWLY ADDED)
-// myProgressBar.addEventListener('change', ()=>{
-//     // Calculate new audio time based on progress bar position
-//     audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
-// });
